a24c1b525f83b45f6b179b1a711c13fad79bad08
elkingutierrex <Pro-egutierrez@intergrupo.com>
2019-12-04 16:05:17 -0500

    294 - Obtener archivos y validarlos

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -15,11 +15,45 @@ export class NgDropFilesDirective {
   @HostListener('dragover', ['$event'])
   public onDragEnter( event:any ){
     this.mouseSobre.emit( true );
+    this._prevenirDetener( event );
   }
 
   @HostListener('dragleave', ['$event'])
   public onDragLeave( event:any ){
     this.mouseSobre.emit( false );
+  }
+
+  @HostListener('drop', ['$event'])
+  public onDrop( event:any ){
+    this.mouseSobre.emit( false );
+
+    const transferencia = this._getTransferencia( event );
+
+    if( !transferencia ){
+      return;
+    }
+    
+    this._extraerArchivos( transferencia.files );
+    this._prevenirDetener( event );
+    this.mouseSobre.emit( false );
+  }
+
+  private _getTransferencia( event:any ){
+    return event.dataTransfer ? event.dataTransfer : event.originalEvent.dataTransfer;
+  }
+
+  private _extraerArchivos( archivosLista: FileList ){
+    
+    for(const propiedad in Object.getOwnPropertyNames( archivosLista )){
+      const archivoTemporal = archivosLista[propiedad];
+      
+      if( this._archivoPuedeSerCargado( archivoTemporal )){
+        const nuevoArchivo = new FileItem( archivoTemporal );
+        this.archivos.push( nuevoArchivo );
+      }
+    }
+
+    console.log( this.archivos );
   }
 
 //validations
